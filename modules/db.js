dc49bef48d56d70d17fc858ac1dfee80c48e3a7e
Milena Oswald <156802363+milenaeastwood@users.noreply.github.com>
2025-03-20 12:50:09 +0100

    db.js comments removed

--- modules/db.js
+++ modules/db.js
@@ -4,23 +4,18 @@ import {del,get,keys,set,values} from './idb-src.min.js';
 export const db = {
     
     readItem : function(key){
-        // liest einen key aus
         return get(key);
     },
     readAll : function (){
-        // gibt alle Werte (als Array) zurück
         return values();
     },
     readKeys : function(){
-         // gibt alle keys (als Array) zurück
         return keys();
     },
     writeItem : function(key,item){
-        // schreibt 1 key:value Paar
         return set(key,item);
     },
     deleteItem : function(key){
-        // löscht einen key inkl. wert
         return del(key);
         
     },
